fe1848ebfa0873f97a44a9c53c3316f466ba1d5b
Natalie Galligan <nat.galligan@gmail.com>
2019-11-13 03:18:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,13 @@ def map(array)
   end
   new_array
 end
+
+def reduce(source_array, starting_point = nil)
+  index = 0
+  new_val = starting_point
+
+  while index < source_array.count do
+    new_val = yield(new_val, source_array[index])
+    index += 1
+  end
+end
